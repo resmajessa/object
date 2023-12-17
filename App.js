e941eef418432a9cb9e12277749b6ad37abf63fc
Jessa Mae <143053099+resmajessa@users.noreply.github.com>
2023-12-17 23:12:49 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet, Alert } from 'react-native';
 
-export default function App() {
+const DATA = [
+  { id: '1', firstName: 'John', lastName: 'Doe', nickname: 'Johnny', course: 'Computer Science', year: '3rd' },
+  { id: '2', firstName: 'Jane', lastName: 'Smith', nickname: 'Janey', course: 'Business', year: '2nd' },
+  { id: '3', firstName: 'Michael', lastName: 'Brown', nickname: 'Mike', course: 'Engineering', year: '1st' },
+
+];
+
+const App = () => {
+  const renderItem = ({ item }) => (
+    <TouchableOpacity
+      style={styles.item}
+      onPress={() => Alert.alert('Full Information', `First Name: ${item.firstName}\nLast Name: ${item.lastName}\nNickname: ${item.nickname}\nCourse: ${item.course}\nYear: ${item.year}`)}
+    >
+      <Text style={styles.title}>{item.nickname}</Text>
+    </TouchableOpacity>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 20,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 24,
   },
 });
+
